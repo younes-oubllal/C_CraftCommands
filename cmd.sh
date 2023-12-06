3a5de34099807879cae803b24eac1af820ac9401
younes-oubllal <138817423+younes-oubllal@users.noreply.github.com>
2023-12-06 23:46:50 +0100

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -3,6 +3,6 @@ echo "source ~/C_Craft/Processing/script/alias.sh" >> ~/.bashrc
 echo "source ~/C_Craft/Processing/script/alias.sh" >> ~/.zshrc
 source ~/.bashrc
 source ~/.zshrc
-exec bash
 clear
 echo -e "\033[32mDownloaded successfully, type \033[34mstart\033[32m to understand how the program works\033[0m".
+exec bash
