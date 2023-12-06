4dc4582d1276d63a66dd624d85425638601ad65f
younes-oubllal <138817423+younes-oubllal@users.noreply.github.com>
2023-12-06 23:34:13 +0100

    Update and rename cmd to cmd.sh

---
+++ cmd.sh
@@ -0,0 +1,6 @@
+(cd ~ && git clone https://github.com/mr-youbella/tt C_Craft) 
+echo "source ~/C_Craft/Processing/script/alias.sh" >> ~/.bashrc
+echo "source ~/C_Craft/Processing/script/alias.sh" >> ~/.zshrc
+source ~/.bashrc
+source ~/.zshrc
+exec bash
