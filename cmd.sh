13949beac723d3bfeae630fa30f00862d1c3f60b
younes-oubllal <138817423+younes-oubllal@users.noreply.github.com>
2023-12-09 00:36:17 +0100

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,4 @@
-(cd ~ && git clone https://github.com/mr-youbella/tt C_Craft) 
+(cd ~ && git clone https://github.com/mr-youbella/C_Craft C_Craft) 
 echo "source ~/C_Craft/Processing/script/alias.sh" >> ~/.bashrc
 echo "source ~/C_Craft/Processing/script/alias.sh" >> ~/.zshrc
 source ~/.bashrc
